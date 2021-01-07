83e0b0fc5d5be4721b29c1c7084a6348701ba4bb
873696121 <873696121@qq.com>
2021-01-07 21:03:49 +0800

    create project javaSourceLearn

---
+++ src/mytest/arraylist/MyTest.java
@@ -0,0 +1,13 @@
+package mytest.arraylist;
+
+import java.util.ArrayList;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            list.add(1);
+        }
+        list.add(1);
+    }
+}
